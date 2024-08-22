19bad04e52c7d77a3239ca896eb4c8203d148969
malgorzb <malgorzb@metropolia.fi>
2024-08-22 11:23:00 +0300

    Second Commit, Interactive demo is created

--- VariableDemo.py
+++ VariableDemo.py
@@ -1,3 +1,20 @@
+num1 = int(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sum = num1 + num2
+print(sum)
+
+name = input("Enter your name: ")
+school = input("Enter your school: ")
+
+print("You are: ", name, " and your school name is:", school)
+
+print(f" your name is,{name}, and your school name is:{school}")
+floatVariable = 4.534763789093245
+print(f"Your name is, {name}, and float Variable is, {floatVariable}")
+print(f"Your name is, {name}, and float Variable is, {floatVariable: .2f}")
+
+
+'''
 intVariable = 4
 floatVariable = 4.5
 stringVariable = "Software 1 is fun"
@@ -9,3 +26,13 @@ print(stringVariable)
 print(type(intVariable))
 print(type(floatVariable))
 print(type(stringVariable))
+
+#typecasting is fun
+intVariable = int(floatVariable)
+print("here is the int version of float variable", intVariable)
+
+shareOfLoan = 500.50/3
+print(shareOfLoan)
+print(int(shareOfLoan))
+print(type(shareOfLoan))
+'''
